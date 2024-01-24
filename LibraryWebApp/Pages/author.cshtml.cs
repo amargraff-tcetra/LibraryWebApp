34964414cbe060ab505598951df2750087cc455a
Aaron Margraff <amargraff@tcetra.com>
2024-01-24 15:36:52 -0500

    Add authors page

---
+++ LibraryWebApp/Pages/author.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace LibraryWebApp.Pages
+{
+    public class authorModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
